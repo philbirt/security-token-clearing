52d14feb92fa0cc6659aa918d84027b9ee1c4d44
Ian Shipman <ian.shipman@cfxtrading.com>
2018-10-25 12:43:45 -0400

    configures testing framework

---
+++ test/PolyMath.ts
@@ -0,0 +1,6 @@
+describe("PolyMath interface", () => {
+  it("should add user represtatation to token");
+  it("should update user representation to token");
+  it("should detect non-KYC user");
+  it("should detect shareholder limit violation");
+});
