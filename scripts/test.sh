e2c696b065e430ebc907daeb6f839afc6130b316
Kevin Solorio <soloriok@gmail.com>
2018-11-01 17:58:54 -0700

    setup polymath
    * ensure submodules have been initialized and updated
    * deploy polymath contracts via truffle
    * copy privKeyLocal and sto config to polymath required for the CLI
    * run the tests
    * kill node (want to update these)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-# launch ganache
+# Launch ganache
+#
+# -l gas limit: set to full
+# -i network id: set to 15 (polymath has this hard coded)
+# -m mnemonic: set to default to keep accounts the same
+#
+# will write std out and error to ganache.log (ignored in git)
 ganache-cli \
   -l 0xffffff \
   -i 15 \
@@ -8,8 +14,27 @@ ganache-cli \
   &> ganache.log &
 
 
-# deploy polymath
+# Setup submodules
+git submodule init && git submodule update
 
+# Change context to polymath directory
+cd polymath-core
 
-# run the tests
+# Deploy contracts to development network
+truffle migrate --network development # need to make sure truffle installed and version < 5
+
+# Change context back to application root
+cd $OLDPWD
+
+# Copy privKeyLocal (polymath requirement) to root of submodule
+cp ./test/support/privKeyLocal polymath-core/privKeyLocal
+
+# Copy STO config to polymath
+cp ./test/support/capped_sto_data.yml polymath-core/CLI/data/capped_sto_data.yml
+
+# Run the tests
+tsc -p tsconfig.test.json && node_modules/.bin/mocha dist-test/* -t 10000
+
+# End ganache process
+killall node #update this to capture the pid of ganache and kill that process
 
