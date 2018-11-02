f0731dbca19c0f937793eedfa46dc07f106f8e45
Kevin Solorio <soloriok@gmail.com>
2018-11-01 17:24:03 -0700

    launch ganache-cli
    * max gas limit
    * set the mnemonic in order to keep the addresses consistent
    * set the id to 15 to work with polymath configs

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# launch ganache
+ganache-cli \
+  -l 0xffffff \
+  -i 15 \
+  -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" \
+  &> ganache.log &
+
+
+# deploy polymath
+
+
+# run the tests
+
